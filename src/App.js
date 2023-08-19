25cba25f482e0ca43cb5433c768cf84d2e4e5441
shashwatwankhedekar <shashwatwankhedekar@gmail.com>
2023-08-19 16:43:59 +0530

    Styling using JS

--- src/App.js
+++ src/App.js
@@ -1,3 +1,10 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 1, description: "Charger", quantity: 1, packed: false },
+  { id: 2, description: "Headphones", quantity: 3, packed: false },
+];
+
 export default function App() {
   return (
     <div className="app">
@@ -20,11 +27,31 @@ function Form() {
   );
 }
 function PackingList() {
-  return <div className="list">LIST</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
 }
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+
 function Stats() {
   return (
-    <footer>
+    <footer className="stats">
       <em>💼 You have X items on your List, and you already packed X (X%)</em>
     </footer>
   );
